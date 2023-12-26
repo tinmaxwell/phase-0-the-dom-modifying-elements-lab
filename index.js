f91c44bf921e182a470707afebd5677624eeaa9e
Max <tinmaxwell@gmail.com>
2023-12-26 12:28:01 -0600

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'Max Tinney is the champion';
+newHeader.id = 'victory';
